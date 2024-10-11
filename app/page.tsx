f388c9f16d17a6dafd857a1d12eedc909d2c9255
kanu6284 <bkanika189@gmail.com>
2024-10-12 01:39:53 +0530

    versal deploy chnges

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default async function Index() {
           <ComparisonPage />
           <ReviewPage />
           <MoneyBackGuarantee />
-          <PricingSection user={user} />
+          <PricingSection />
           <DataSecuritySection />
           <FAQSection />
           <HeadshotContainer />
