fc86d3c110eb69b24f87493ce87bda92ff71393f
kanu6284 <bkanika189@gmail.com>
2024-10-22 15:20:46 +0530

    final copy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['x3rkl8tapynlmqus.public.blob.vercel-storage.com'],
-  },
 }
 
 module.exports = nextConfig
