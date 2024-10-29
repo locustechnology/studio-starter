0185130acbce202f6a138211b371c0e010f2db81
Locus Tech Admin <locustechnology@proton.me>
2024-10-30 02:17:32 +0530

    removed the front-end fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "@/styles/fonts.css";
 import { Suspense } from "react";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
@@ -16,6 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }: any) {
   return (
     <html lang="en" className="overflow-x-hidden">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Plus+Jakarta+Sans:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
       <body className="flex flex-col bg-[#F4F7FA] min-h-screen w-full overflow-x-hidden font-[Poppins]">
         <Navbar />
         <main className="flex-1 flex flex-col w-full">
