4aa89d36b6b4bb8484d0e3365b24f02afbe562e5
Locus Tech Admin <locustechnology@proton.me>
2024-10-30 16:32:54 +0530

    Revert "Application Error on Vercel"
    This reverts commit e02384fe2ff7f9999fc254620b28f7b4a5082d7f.

--- next.config.js
+++ next.config.js
@@ -9,16 +9,7 @@ const nextConfig = {
     domains: [
       'x3rkl8tapynlmqus.public.blob.vercel-storage.com'
     ],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        'supports-color': false
-      };
-    }
-    return config;
-  },
+  }
 }
 
 module.exports = nextConfig
